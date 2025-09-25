9fc76e9fcf1885203c82422f3697dd7720ce5057
laragarciaogueta <laragosb@gmail.com>
2025-09-25 13:47:18 +0000

    a

--- js/extraterrestre.js
+++ js/extraterrestre.js
@@ -1,12 +1,14 @@
-import {Jugador} from "./jugador.js"
+import { Jugador } from "./jugador.js";
 
-export class Extraterrestre extends Jugador{
-    constructor(nombre){
-        const fuerza = Math.floor(Math.random() * 100) + 1; // 1-100
+export class Extraterrestre extends Jugador {
+    constructor(nombre) {
+        const fuerza = Math.floor(Math.random() * 100) + 1;
         super(nombre, fuerza, 150);
+        this._nombre = nombre; // almacenamos el nombre original
     }
-   
-    getNombre(){
-        return this.nombre.split("").reverse().join("");
+
+    get nombre() {
+        return this._nombre.split("").reverse().join("");
     }
 }
+
