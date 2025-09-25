a808db26af3ddb264c1ea9dc008667e99e76c41f
laragarciaogueta <laragosb@gmail.com>
2025-09-25 12:39:29 +0000

    clase jugador

---
+++ js/jugador.js
@@ -0,0 +1,24 @@
+export {Jugador}
+class Jugador{
+    //constructora
+    constructor(miNombre, fuerza, salud){
+        this.miNombre = miNombre
+        this.fuerza = fuerza
+        this.salud = salud
+    }
+    
+    //metodo getNombre()
+    getNombre(){
+        return this.miNombre
+    }
+
+    //método luchar(jugador)
+    luchar(jugador){
+        if ( Math.random()* this.fuerza > jugador.fuerza ){ jugador.salud -= this.fuerza;
+        } else {
+        this.salud -= jugador.fuerza;
+        }
+    }
+
+}
+    
